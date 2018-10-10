faae4c735029bb815477ce9dd142e53e05ad3ca4
Bingo Bobo <matmazur90@gmail.com>
2018-10-10 11:59:15 +0200

    created package batch/batchlet and created first dummy batchlet

---
+++ src/main/java/batch/batchlet/CopyFilesBatchlet.java
@@ -0,0 +1,12 @@
+package batch.batchlet;
+
+import javax.batch.api.AbstractBatchlet;
+import javax.inject.Named;
+
+@Named
+public class CopyFilesBatchlet extends AbstractBatchlet {
+
+    public String process() throws Exception {
+        return null;
+    }
+}
